7f5a3acbb85ea516bbb1cae31f108ad81bd3da13
naval88 <navalkishor2005@gmail.com>
2023-08-18 13:10:48 +0530

    First commit in

---
+++ app.js
@@ -0,0 +1,5 @@
+const heading = React.createElement('h1', {id:'abc',xyz:'naval'}, 'Hello namaster React');
+const root =  ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
+
+    
